d0a36bda4b90763dc26836654253cb94c7d4e483
chair28980 <vrycmfy@protonmail.com>
2023-09-30 02:02:37 -0700

    add links to issues

--- get_milestones.rb
+++ get_milestones.rb
@@ -9,6 +9,10 @@ def query_repo
     repo = "waku-org/pm"
     milestones = client.list_milestones(repo)
 
+    def extract_links(body)
+      body.scan(/https?:\/\/[\S]+/).uniq
+    end
+
     milestones.each do |milestone|
       puts "Milestone: #{milestone.title}"
       puts "Link: #{milestone.html_url}"
@@ -17,8 +21,15 @@ def query_repo
       issues = client.list_issues(repo, milestone: milestone.number)
     
       issues.each do |issue|
-        puts "Issue: #{issue.title}"
+        puts "Epic: #{issue.title}"
         puts "Link: #{issue.html_url}"
+        links = extract_links(issue.body)
+        if links.any?
+          puts "Issues in Epic:"
+          links.each { |link| puts link }
+        else
+          puts "No issues in Epic description."
+        end
         puts "----------------------------"
       end
       puts "\n"
